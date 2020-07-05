5448b53c635047b48d71babf9a7ac134678b94b6
David Tierney <d.r.tierney@hotmail.co.uk>
2020-07-05 12:12:59 +0100

    Stage #4 completed
    Stage #4 A good stack
    Learn to use hashtables, display key values and work with exceptions in order to fix the problem of repeating cards.

--- Flashcards/task/src/flashcards/Main.java
+++ Flashcards/task/src/flashcards/Main.java
@@ -8,6 +8,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Map<String, String> flashcards = new LinkedHashMap<>();
+        String aTerm = "";
+        String aDefinition = "";
 
         System.out.print("Input the number of cards:\n");
         int cards = Integer.parseInt(scanner.nextLine());
@@ -15,10 +17,22 @@ public class Main {
         int card = 0;
         while (card < cards) {
             System.out.printf("The card #%d:\n", card + 1);
-            String term = scanner.nextLine();
+            do {
+                aTerm = scanner.nextLine();
+                if (flashcards.containsKey(aTerm)){
+                    System.out.printf("The card \"%s\" already exists. Try again:\n", aTerm);
+                }
+            } while (flashcards.containsKey(aTerm) || aTerm.equals(""));
+
             System.out.printf("The definition of card #%d:\n", card + 1);
-            String definition = scanner.nextLine();
-            flashcards.putIfAbsent(term, definition);
+            do {
+                aDefinition = scanner.nextLine();
+                if(flashcards.containsValue(aDefinition)){
+                    System.out.printf("The definition \"%s\" already exists. Try again:\n", aDefinition);
+                }
+            } while (flashcards.containsValue(aDefinition) || aDefinition.equals(""));
+
+            flashcards.putIfAbsent(aTerm, aDefinition);
             card++;
         }
 
@@ -26,8 +40,21 @@ public class Main {
             String definition = flashcards.get(term);
             System.out.printf("Print the definition of \"%s\":\n", term);
             String answer = scanner.nextLine();
-            System.out.printf(answer.equalsIgnoreCase(definition) ?
-                    "Correct answer.\n" : "Wrong answer. The correct one is \"%s\".\n", definition);
+            System.out.printf(answer.equalsIgnoreCase(definition) ? "Correct answer.\n" :
+            flashcards.containsValue(answer) ?
+            "Wrong answer. The correct one is \"%1s\", you've just written the definition of \"%2s\" \n" :
+            "Wrong answer. The correct one is \"%1s\".\n",
+            definition, getKeyFromValue(flashcards, answer));
         }
+
+    }
+
+    public static String getKeyFromValue(Map<String, String> map, String value) {
+        for (var entry : map.entrySet()) {
+            if (entry.getValue().equals(value)) {
+                return entry.getKey();
+            }
+        }
+        return null;
     }
 }
